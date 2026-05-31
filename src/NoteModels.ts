e6dbb9d7977e5be4d37366e337472568f73ab6d2
oluchi <anakorafav@gmail.com>
2026-05-31 09:45:07 +0100

    created category interface and model

--- src/NoteModels.ts
+++ src/NoteModels.ts
@@ -8,14 +8,6 @@ export interface INote extends Document {
   updatedAt: Date;
 }
 
-// this is the category interface
-export interface ICategory extends Document {
-  name: string;
-  description: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
 // this is the mongoose schema that defines how the Note document will be stored in the database
 const noteSchema = new Schema<INote>(
   {
@@ -36,25 +28,6 @@ const noteSchema = new Schema<INote>(
   { timestamps: true }, // auto-manage createdAt and updatedAt fields
 );
 
-// this is the mongoose schema that defines how the Category document will be stored in the database
-const categorySchema = new Schema<ICategory>(
-  {
-    name: {
-      type: String,
-      required: true,
-      minlength: [2, "Category name must be at least 2 characters long"],
-      maxlength: [50, "Category name cannot exceed 50 characters"],
-    },
-    description: {
-      type: String,
-      required: true,
-      minlength: [5, "Category description must be at least 5 characters long"],
-    },
-  },
-  { timestamps: true }, // auto-manage createdAt and updatedAt fields
-);
-
 const Note = mongoose.model<INote>("Note", noteSchema);
-const Category = mongoose.model<ICategory>("Category", categorySchema);
 
-export default { Note, Category };
+export default Note;
